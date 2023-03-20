b2fa63afa5a0026183e97f8beabe98a20555d32f
Multi-Illusion <54984479+Multi-Illusion@users.noreply.github.com>
2023-03-20 21:52:10 +0800

    Create build.rs

---
+++ build.rs
@@ -0,0 +1,11 @@
+fn main() {
+    slint_build::compile_with_config(
+        "ui/appwindow.slint",
+        slint_build::CompilerConfiguration::new()
+            .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
+    )
+    .unwrap();
+    println!("cargo:rustc-link-arg-bins=--nmagic");
+    println!("cargo:rustc-link-arg-bins=-Tlink.x");
+    println!("cargo:rustc-link-arg-bins=-Tdefmt.x");
+}
