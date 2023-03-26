9a0a7f452772070460765d196080e9546fc09c84
Multi-Illusion <54984479+Multi-Illusion@users.noreply.github.com>
2023-03-26 16:20:41 +0800

    format project name

--- build.rs
+++ build.rs
@@ -1,6 +1,6 @@
 fn main() {
     slint_build::compile_with_config(
-        "ui/appwindow.slint",
+        "ui/multigrip.slint",
         slint_build::CompilerConfiguration::new()
             .embed_resources(slint_build::EmbedResourcesKind::EmbedForSoftwareRenderer),
     )
